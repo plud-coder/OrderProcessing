79312cd7309f587a08fa4ab390e4eebcdb20d763
PLUD <itthiphat.ma@ku.th>
2026-08-28 16:00:06 +0700

    Add files via upload

--- src/PercentageDiscount.java
+++ src/PercentageDiscount.java
@@ -12,6 +12,8 @@ public class PercentageDiscount implements DiscountStrategy {
         double total = order.getTotalPrice();
         // TODO (2a): คืนราคาหลังหักส่วนลด percent%
         //   hint: total - (total * percent / 100.0)
-        return /* ====== replace this ====== */ total;
+        
+        //total = (100 - percent) / 100 * total; 
+        return total = total - (total* percent / 100.0);
     }
 }
